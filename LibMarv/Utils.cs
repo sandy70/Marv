e34342d20afdafc7b9274be519173b252c8b63f2
Vinod Khare <vinod.khare@gmail.com>
2013-04-29 11:38:47 -0400

    Some extensions and Utils moved to LibMarv.

---
+++ LibMarv/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LibMarv
+{
+    public static class Utils
+    {
+        public static double Distance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+    }
+}
