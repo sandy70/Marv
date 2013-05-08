a6e8f9f5ca8abc1b26e6bbe4c13f87a19730b954
Vinod Khare <vinod.khare@gmail.com>
2013-05-08 16:27:52 -0400

    ILocation: added.

---
+++ LibPipeline/ILocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibPipeline
+{
+    interface ILocation
+    {
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+    }
+}
---
+++ LibPipeline/MultiLocationReader.cs
@@ -0,0 +1,44 @@
+﻿using LinqToExcel;
+using MapControl;
+using System;
+using System.Collections.Generic;
+
+namespace LibPipeline
+{
+    public class MultiLocationReader
+    {
+        public static List<Location> ReadExcel(string fileName, string sheetName = "Sheet1")
+        {
+            var excel = new ExcelQueryFactory(fileName);
+            var locations = new List<Location>();
+
+            var colNames = excel.GetColumnNames(sheetName);
+
+            foreach (var row in excel.Worksheet(sheetName))
+            {
+                if (DBNull.Value.Equals(row["Latitude"].Value) || DBNull.Value.Equals(row["Longitude"].Value))
+                {
+                    continue;
+                }
+
+                var location = new MapControl.Location();
+
+                foreach (var colName in colNames)
+                {
+                    if (colName.Equals("Latitude"))
+                    {
+                        location.Latitude = (double)row[colName].Value;
+                    }
+                    else if (colName.Equals("Longitude"))
+                    {
+                        location.Longitude = (double)row[colName].Value;
+                    }
+                }
+
+                locations.Add(location);
+            }
+
+            return locations;
+        }
+    }
+}
