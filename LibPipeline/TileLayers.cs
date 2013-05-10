7cc522a9599e824382da2bb442df71446dbedadd
Vinod Khare <vinod.khare@gmail.com>
2013-05-10 09:19:32 -0400

    TileLayers: static class to hold all the tile layers available.

---
+++ LibPipeline/TileLayers.cs
@@ -0,0 +1,21 @@
+﻿using MapControl;
+
+namespace LibPipeline
+{
+    public static class TileLayers
+    {
+        public static TileLayer MapQuestSat = new TileLayer
+        {
+            SourceName = "MapQuestSat",
+            Description = "MapQuest Satellite",
+            TileSource = new TileSource("http://otile{n}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png")
+        };
+
+        public static TileLayer OsmMap = new TileLayer
+        {
+            SourceName = "OsmMap",
+            Description = "OpenStreetMap Map",
+            TileSource = new TileSource("http://{c}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+        };
+    }
+}
