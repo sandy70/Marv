169cab8bdcf55f06bdcbc3a0578d50aeead21259
Vinod Khare <vinod.khare@gmail.com>
2013-05-16 12:25:45 -0400

    LibBn: classes to read a network structure from a network file.

---
+++ LibBn/NetworkStructure.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LibBn
+{
+    public class NetworkStructure
+    {
+        public List<string> Footer = new List<string>();
+
+        public List<NetworkStructureNode> Nodes = new List<NetworkStructureNode>();
+
+        public Dictionary<string, string> Properties = new Dictionary<string, string>();
+
+        public static NetworkStructure Read(string path)
+        {
+            var structure = new NetworkStructure();
+
+            var fileLines = File.ReadAllLines(path).Trimmed().ToList();
+
+            var nLines = fileLines.Count;
+
+            for (int i = 0; i < nLines; i++)
+            {
+                if (fileLines[i].StartsWith("node"))
+                {
+                    var parts = fileLines[i].Split(new char[] { ' ' }, 2).ToList();
+
+                    var node = new NetworkStructureNode { Key = parts[1] };
+
+                    structure.Nodes.Add(node);
+
+                    while (!fileLines[i].Equals("{"))
+                    {
+                        i++;
+                    }
+
+                    i++;
+
+                    while (!fileLines[i].Equals("}"))
+                    {
+                        parts = fileLines[i].Split(new char[] { '=', ';' }, 2, StringSplitOptions.RemoveEmptyEntries).ToList();
+                        node.Properties[parts[0].Trim()] = new string(parts[1].Trim().AllButLast().ToArray());
+                        i++;
+                    }
+                }
+
+                else if (fileLines[i].Equals("net"))
+                {
+                    while (!fileLines[i].Equals("{"))
+                    {
+                        i++;
+                    }
+
+                    i++;
+
+                    while (!fileLines[i].Equals("}"))
+                    {
+                        var parts = fileLines[i].Split(new char[] { '=', ';' }, 2, StringSplitOptions.RemoveEmptyEntries).ToList();
+                        structure.Properties[parts[0].Trim()] = new string(parts[1].Trim().AllButLast().ToArray());
+                        i++;
+                    }
+                }
+
+                else
+                {
+                    structure.Footer.Add(fileLines[i]);
+                }
+            }
+
+            foreach (var node in structure.Nodes)
+            {
+                var linkValueString = node.Properties["HR_LinkMode"];
+                var parts = linkValueString.Split(new char[] { '[', ']', '"' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var part in parts)
+                {
+                    var subParts = part.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    node.Children.Add(structure.GetNode(subParts[0]));
+                }
+            }
+
+            return structure;
+        }
+
+        public void Write(string path)
+        {
+            using (var writer = new StreamWriter(path))
+            {
+                writer.WriteLine("net");
+                writer.WriteLine("{");
+
+                foreach (var prop in this.Properties)
+                {
+                    writer.WriteLine("\t{0} = {1};", prop.Key, prop.Value);
+                }
+
+                writer.WriteLine("}");
+
+                foreach (var node in this.Nodes)
+                {
+                    writer.WriteLine();
+                    writer.WriteLine("node {0}", node.Key);
+                    writer.WriteLine("{");
+
+                    foreach (var prop in node.Properties)
+                    {
+                        writer.WriteLine("\t{0} = {1};", prop.Key, prop.Value);
+                    }
+
+                    writer.WriteLine("}");
+                }
+
+                writer.WriteLine();
+
+                foreach (var line in this.Footer)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        public void FixStates()
+        {
+            foreach (var node in this.Nodes)
+            {
+                var states = node.ParseStates();
+
+                foreach (var prop in node.Properties.Where(x => x.Key.StartsWith("HR_State_")).ToList())
+                {
+                    node.Properties.Remove(prop.Key);
+                }
+
+                foreach (var state in states)
+                {
+                    int n = states.IndexOf(state);
+                    node.Properties[String.Format("HR_State_{0}", n)] = "\"" + state + "\"";
+                }
+            }
+        }
+    }
+}
---
+++ LibBn/NetworkStructureNode.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibBn
+{
+    public class NetworkStructureNode
+    {
+        public string Key = "";
+        public Dictionary<string, string> Properties = new Dictionary<string, string>();
+
+        internal List<string> ParseStates()
+        {
+            var states = new List<string>();
+            var subtype = "";
+
+            if (this.Properties.TryGetValue("subtype", out subtype))
+            {
+                if (subtype.Equals("interval"))
+                {
+                    var stateStrings = this.Properties["state_values"]
+                                           .Split(new char[] { '(', ')', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                           .ToList();
+
+                    var nStatesStrings = stateStrings.Count;
+
+                    for (int i = 0; i < nStatesStrings - 1; i++)
+                    {
+                        states.Add(String.Format("{0} - {1}", stateStrings[i], stateStrings[i + 1]));
+                    }
+                }
+            }
+            else
+            {
+                var statesString = "";
+                var stateString = "";
+
+                if(this.Properties.TryGetValue("states", out statesString))
+                {
+                    bool isReading = false;
+                    foreach(var c in statesString.ToCharArray())
+                    {
+                        if(c == '"')
+                        {
+                            if(isReading)
+                            {
+                                states.Add(stateString);
+                                stateString = "";
+                            }
+
+                            isReading = !isReading;
+                            continue;
+                        }
+
+                        if(isReading)
+                        {
+                            stateString += c;
+                        }
+                    }
+                }
+                else
+                {
+                    // something is really really wrong. We could flag errors here.
+                }
+            }
+
+            return states;
+        }
+    }
+}
