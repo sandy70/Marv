dfb34136b1c77d844ed237f4cab4bdd35bd15f52
Vinod Khare <vinod.khare@gmail.com>
2013-05-16 14:58:20 -0400

    Extensions: bunch of functions.

--- LibBn/Extensions.cs
+++ LibBn/Extensions.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using Smile;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
+using System.Windows;
 
 namespace LibBn
 {
     public static class Extensions
     {
+        public static IEnumerable<T> AllButLast<T>(this IEnumerable<T> source)
+        {
+            var it = source.GetEnumerator();
+            bool hasRemainingItems = false;
+            bool isFirst = true;
+            T item = default(T);
+
+            do
+            {
+                hasRemainingItems = it.MoveNext();
+                if (hasRemainingItems)
+                {
+                    if (!isFirst) yield return item;
+                    item = it.Current;
+                    isFirst = false;
+                }
+            } while (hasRemainingItems);
+        }
+
+        public static IEnumerable<T> AllButLastN<T>(this IEnumerable<T> source, int n)
+        {
+            var it = source.GetEnumerator();
+            bool hasRemainingItems = false;
+            var cache = new Queue<T>(n + 1);
+
+            do
+            {
+                if (hasRemainingItems = it.MoveNext())
+                {
+                    cache.Enqueue(it.Current);
+                    if (cache.Count > n)
+                        yield return cache.Dequeue();
+                }
+            } while (hasRemainingItems);
+        }
+
         public static BnVertexInput Find(this Dictionary<int, List<BnVertexInput>> vertexInputsByYear, int year, string key)
         {
             var vertexInputs = vertexInputsByYear.SingleOrDefault(x => x.Key == year).Value;
@@ -32,6 +71,42 @@ namespace LibBn
             return null;
         }
 
+        public static ObservableCollection<BnState> ParseStates(this Network network, string key)
+        {
+            var states = new ObservableCollection<BnState>();
+
+            var nodeHandle = network.GetNodeHandle(key);
+
+            int nStates = network.GetOutcomeCount(nodeHandle);
+            string[] names = new string[nStates];
+            double[] values = network.GetNodeValue(nodeHandle);
+
+            foreach (var prop in network.GetNodeUserProperties(nodeHandle))
+            {
+                if (prop.name.Contains("HR_State_"))
+                {
+                    int stateIndex;
+                    int.TryParse(prop.name.Substring(9), out stateIndex);
+
+                    if (stateIndex < nStates)
+                    {
+                        names[stateIndex] = prop.value;
+                    }
+                }
+            }
+
+            for (int s = 0; s < nStates; s++)
+            {
+                states.Add(new BnState
+                {
+                    Key = names[s],
+                    Value = values[s]
+                });
+            }
+
+            return states;
+        }
+
         public static void Remove(this Dictionary<int, List<BnVertexInput>> vertexInputsByYear, int year, string key)
         {
             var vertexInputs = vertexInputsByYear.SingleOrDefault(x => x.Key == year).Value;
@@ -44,6 +119,61 @@ namespace LibBn
             {
                 vertexInputs.RemoveAll(x => x.Key == key);
             }
+        }
+
+        public static string String(this IEnumerable<string> strings)
+        {
+            var str = "";
+
+            if (strings.Count() == 0)
+            {
+                return str;
+            }
+            else if (strings.Count() == 1)
+            {
+                str += strings.First();
+                return str;
+            }
+
+            foreach (var s in strings.AllButLast())
+            {
+                str += s + ",";
+            }
+
+            str += strings.Last();
+
+            return str;
+        }
+
+        public static string String(this Dictionary<string, Point> positionsByGroup)
+        {
+            var str = "";
+
+            if (positionsByGroup.Count == 0)
+            {
+                return str;
+            }
+            else if (positionsByGroup.Count == 1)
+            {
+                var kvpFirst = positionsByGroup.First();
+                str += kvpFirst.Key + "," + kvpFirst.Value.X + "," + kvpFirst.Value.Y;
+                return str;
+            }
+
+            foreach (var kvp in positionsByGroup.AllButLast())
+            {
+                str += kvp.Key + "," + kvp.Value.X + "," + kvp.Value.Y + ",";
+            }
+
+            var kvpLast = positionsByGroup.Last();
+            str += kvpLast.Key + "," + kvpLast.Value.X + "," + kvpLast.Value.Y;
+
+            return str;
+        }
+
+        public static IEnumerable<string> Trimmed(this IEnumerable<string> untrimmed)
+        {
+            return untrimmed.Select(x => x.Trim());
         }
 
         private static T DeepClone<T>(this T original, Dictionary<Object, Object> copies, params Object[] args)
@@ -152,42 +282,6 @@ namespace LibBn
                 }
             }
             return false;
-        }
-
-        public static IEnumerable<T> AllButLast<T>(this IEnumerable<T> source)
-        {
-            var it = source.GetEnumerator();
-            bool hasRemainingItems = false;
-            bool isFirst = true;
-            T item = default(T);
-
-            do
-            {
-                hasRemainingItems = it.MoveNext();
-                if (hasRemainingItems)
-                {
-                    if (!isFirst) yield return item;
-                    item = it.Current;
-                    isFirst = false;
-                }
-            } while (hasRemainingItems);
-        }
-
-        public static IEnumerable<T> AllButLastN<T>(this IEnumerable<T> source, int n)
-        {
-            var it = source.GetEnumerator();
-            bool hasRemainingItems = false;
-            var cache = new Queue<T>(n + 1);
-
-            do
-            {
-                if (hasRemainingItems = it.MoveNext())
-                {
-                    cache.Enqueue(it.Current);
-                    if (cache.Count > n)
-                        yield return cache.Dequeue();
-                }
-            } while (hasRemainingItems);
         }
     }
 }
