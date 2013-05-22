367e1b0c289bed86fa77f24ab2e1bdc7a5517c5e
Vinod Khare <vinod.khare@gmail.com>
2013-05-22 10:28:28 -0400

    ValueStore: added HasLocationValue(), SetLocationValue().

--- LibPipeline/ValueStore.cs
+++ LibPipeline/ValueStore.cs
@@ -1,30 +1,17 @@
-﻿using LibBn;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace LibPipeline
 {
     public class ValueStore
     {
-        private Dictionary<Location, Dictionary<int, Dictionary<string, Dictionary<string, double>>>> pipelineValue = 
-            new Dictionary<Location, Dictionary<int, Dictionary<string, Dictionary<string, double>>>>();
-        
-        private Location defaultLocation = new Location();
+        private ILocation defaultLocation = new Location();
 
-        public Dictionary<int, Dictionary<string, Dictionary<string, double>>> GetLocationValue(Location location)
-        {
-            if (this.pipelineValue.ContainsKey(location))
-            {
-                return this.pipelineValue[location];
-            }
-            else
-            {
-                return this.pipelineValue[location] = new Dictionary<int, Dictionary<string, Dictionary<string, double>>>();
-            }
-        }
+        private Dictionary<ILocation, Dictionary<int, Dictionary<string, Dictionary<string, double>>>> pipelineValue =
+            new Dictionary<ILocation, Dictionary<int, Dictionary<string, Dictionary<string, double>>>>();
 
-        public Dictionary<string, Dictionary<string, double>> GetGraphValue(int year, Location location = null)
+        public Dictionary<string, Dictionary<string, double>> GetGraphValue(int year, ILocation location = null)
         {
-            if(location == null) location = this.defaultLocation;
+            if (location == null) location = this.defaultLocation;
 
             var locationValue = this.GetLocationValue(location);
 
@@ -38,23 +25,19 @@ namespace LibPipeline
             }
         }
 
-        public Dictionary<string, double> GetVertexValue(string key, int year = int.MinValue, Location location = null)
+        public Dictionary<int, Dictionary<string, Dictionary<string, double>>> GetLocationValue(ILocation location)
         {
-            if (location == null) location = this.defaultLocation;
-
-            var graphValue = this.GetGraphValue(year, location);
-
-            if (graphValue.ContainsKey(key))
+            if (this.pipelineValue.ContainsKey(location))
             {
-                return graphValue[key];
+                return this.pipelineValue[location];
             }
             else
             {
-                return graphValue[key] = new Dictionary<string, double>();
+                return this.pipelineValue[location] = new Dictionary<int, Dictionary<string, Dictionary<string, double>>>();
             }
         }
 
-        public double GetStateValue(string stateKey, string vertexKey = "__DEFAULT__", int year = int.MinValue, Location location = null)
+        public double GetStateValue(string stateKey, string vertexKey = "__DEFAULT__", int year = int.MinValue, ILocation location = null)
         {
             if (location == null) location = this.defaultLocation;
 
@@ -70,7 +53,49 @@ namespace LibPipeline
             }
         }
 
-        public void SetStateValue(double value, string stateKey, string vertexKey = "__DEFAULT__", int year = int.MinValue, Location location = null)
+        public Dictionary<string, double> GetVertexValue(string key, int year = int.MinValue, ILocation location = null)
+        {
+            if (location == null) location = this.defaultLocation;
+
+            var graphValue = this.GetGraphValue(year, location);
+
+            if (graphValue.ContainsKey(key))
+            {
+                return graphValue[key];
+            }
+            else
+            {
+                return graphValue[key] = new Dictionary<string, double>();
+            }
+        }
+
+        public bool HasLocationValue(ILocation location)
+        {
+            if (this.pipelineValue.ContainsKey(location))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SetGraphValue(Dictionary<string, Dictionary<string, double>> graphValue, int year, ILocation location = null)
+        {
+            if (location == null) location = this.defaultLocation;
+
+            var locationValue = this.GetLocationValue(location);
+
+            locationValue[year] = graphValue;
+        }
+
+        public void SetLocationValue(Dictionary<int, Dictionary<string, Dictionary<string, double>>> locationValue, ILocation location)
+        {
+            this.pipelineValue[location] = locationValue;
+        }
+
+        public void SetStateValue(double value, string stateKey, string vertexKey = "__DEFAULT__", int year = int.MinValue, ILocation location = null)
         {
             if (location == null) location = this.defaultLocation;
 
