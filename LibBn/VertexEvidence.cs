0ed6c360660ab1acc5f6be475bd9101b8c2e910f
Vinod Khare <vinod.khare@gmail.com>
2013-05-22 10:38:52 -0400

    VertexEvidence: class to model vertex evidence.

---
+++ LibBn/VertexEvidence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBn
+{
+    public class VertexEvidence
+    {
+        public EvidenceType EvidenceType;
+        public double[] Evidence;
+        public int StateIndex;
+    }
+}
