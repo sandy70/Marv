f7c7ac2b9d6c27c77b3a84e278dd3eb74e363710
Vinod Khare <vinod.khare@gmail.com>
2013-05-30 09:38:34 -0400

    BnState: add properties Min and Max.

--- LibBn/BnState.cs
+++ LibBn/BnState.cs
@@ -8,6 +8,8 @@ namespace LibBn
     {
         private double _value;
         private string key;
+        private double max = double.MinValue;
+        private double min = double.MinValue;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -24,6 +26,42 @@ namespace LibBn
                 {
                     this.key = value;
                     this.OnPropertyChanged("Key");
+                }
+            }
+        }
+
+        public double Max
+        {
+            get
+            {
+                return this.max;
+            }
+
+            set
+            {
+                if (value != this.max)
+                {
+                    this.max = value;
+
+                    this.OnPropertyChanged("Max");
+                }
+            }
+        }
+
+        public double Min
+        {
+            get
+            {
+                return this.min;
+            }
+
+            set
+            {
+                if (value != this.min)
+                {
+                    this.min = value;
+
+                    this.OnPropertyChanged("Min");
                 }
             }
         }
