572cc07b0fa50b83547701af8a97d1234cc33a17
Vinod Khare <vinod.khare@gmail.com>
2013-06-06 16:18:19 -0400

    LocationValue: models the value for a pipeline location over several years.

---
+++ LibBn/LocationValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBn
+{
+    public class LocationValue : Dictionary<int, ModelValue>
+    {
+        public ModelValue GetModelValue(int year)
+        {
+            if(this.HasModelValue(year))
+            {
+                return this[year];
+            }
+            else
+            {
+                return this[year] = new ModelValue();
+            }
+        }
+
+        public bool HasModelValue(int year)
+        {
+            return this.ContainsKey(year);
+        }
+    }
+}
