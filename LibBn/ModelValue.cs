a8fd51e53eb3693d461f2f85acc4f4289aecd5fb
Vinod Khare <vinod.khare@gmail.com>
2013-06-06 16:18:40 -0400

    ModelValue: models the value of a model consisting of several graphs.

---
+++ LibBn/ModelValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBn
+{
+    public class ModelValue : Dictionary<string, GraphValue>
+    {
+        public GraphValue GetGraphValue(string graphName)
+        {
+            if (this.HasGraphValue(graphName))
+            {
+                return this[graphName];
+            }
+            else
+            {
+                return this[graphName] = new GraphValue();
+            }
+        }
+
+        public void SetGraphValue(string graphName, GraphValue graphValue)
+        {
+            this[graphName] = graphValue;
+        }
+
+        public bool HasGraphValue(string graphName)
+        {
+            return this.ContainsKey(graphName);
+        }
+    }
+}
