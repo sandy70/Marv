7a19508899f51a06ebe2c252026b07fc6c23a480
Vinod Khare <vinod.khare@gmail.com>
2013-06-06 16:20:06 -0400

    PipelineValue: modesl the value of the network over a whole pipeline.

---
+++ LibPipeline/PipelineValue.cs
@@ -0,0 +1,22 @@
+﻿using LibBn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibPipeline
+{
+    public class PipelineValue : Dictionary<ILocation, LocationValue>
+    {
+        public bool HasLocationValue(ILocation location)
+        {
+            return this.ContainsKey(location);
+        }
+
+        public LocationValue GetLocationValue(ILocation location)
+        {
+            return this[location];
+        }
+    }
+}
