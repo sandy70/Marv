938800a5be0a6acdcc36c259ea5ba4c56beb348b
Vinod Khare <vinod.khare@gmail.com>
2013-06-06 16:17:49 -0400

    GraphValue: models the value for a BnGraph.

---
+++ LibBn/GraphValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBn
+{
+    public class GraphValue : Dictionary<string, VertexValue>
+    {
+        public VertexValue GetVertexValue(string vertexKey)
+        {
+            if (this.HasVertexValue(vertexKey))
+            {
+                return this[vertexKey];
+            }
+            else
+            {
+                return this[vertexKey] = new VertexValue();
+            }
+        }
+
+        public bool HasVertexValue(string vertexKey)
+        {
+            return this.ContainsKey(vertexKey);
+        }
+    }
+}
