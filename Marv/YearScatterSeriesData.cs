87ae5443e96422a2f597729f806a455987181864
Vinod Khare <vinod.khare@gmail.com>
2013-06-25 14:36:49 -0400

    YearScatterSeriesData: models the data for a scatter series for a given year.

---
+++ Marv/YearScatterSeriesData.cs
@@ -0,0 +1,24 @@
+﻿namespace Marv
+{
+    public class YearScatterSeriesData : MultiPoint
+    {
+        private int year;
+
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+
+            set
+            {
+                if (value != this.year)
+                {
+                    this.year = value;
+                    this.OnPropertyChanged("Year");
+                }
+            }
+        }
+    }
+}
