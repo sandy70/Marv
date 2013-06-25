a275bfb0b3658d88bc8ea3005f50db9a45695fb0
Vinod Khare <vinod.khare@gmail.com>
2013-06-25 16:44:03 -0400

    ViewModel: base class for any view model. Provides concrete implementation of INotifyPropertyChanged.

---
+++ Marv/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Marv
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
