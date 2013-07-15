6c57d0e298f238fcb9b4260c037c6bb1f621baf3
Vinod Khare <vinod.khare@gmail.com>
2013-07-15 14:22:07 -0400

    MultiLocation: models a selectable collection of Locations.

---
+++ LibPipeline/Geometry/MultiLocation.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace LibPipeline
+{
+    public class MultiLocation : SelectableCollection<Location>
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value != this.name)
+                {
+                    this.name = value;
+                    this.OnPropertyChanged(new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+    }
+}
