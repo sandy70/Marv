0d947bd6bedd3ee24ff693f93b61a67c3fec7a4f
Vinod Khare <vinod.khare@gmail.com>
2013-07-18 14:28:21 -0400

    SelectableCollection: automatically select first object on add.

--- LibPipeline/SelectableCollection.cs
+++ LibPipeline/SelectableCollection.cs
@@ -1,13 +1,31 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 
 namespace LibPipeline
 {
-    public class SelectableCollection<T> : ObservableCollection<T>, INotifyPropertyChanged
+    public class SelectableCollection<T> : ObservableCollection<T>
     {
+        private bool isFirstSelectedOnAdd = true;
         private T selectedItem = default(T);
+
+        public bool IsFirstSelectedOnAdd
+        {
+            get
+            {
+                return this.isFirstSelectedOnAdd;
+            }
+
+            set
+            {
+                if (value != this.isFirstSelectedOnAdd)
+                {
+                    this.isFirstSelectedOnAdd = value;
+                    this.OnPropertyChanged(new PropertyChangedEventArgs("IsFirstSelectedOnAdd"));
+                }
+            }
+        }
 
         public T SelectedItem
         {
@@ -57,6 +75,43 @@ namespace LibPipeline
                     {
                         this.SelectedItem = default(T);
                     }
+                }
+            }
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnCollectionChanged(e);
+
+            if (!this.IsFirstSelectedOnAdd)
+            {
+                return;
+            }
+
+            if (e.NewItems != null && this.Count == 1)
+            {
+                this.SelectedItem = this.First();
+            }
+
+            else if (e.OldItems != null)
+            {
+                bool selectedRemoved = false;
+
+                foreach (var item in e.OldItems)
+                {
+                    if (item.Equals(this.SelectedItem))
+                    {
+                        selectedRemoved = true;
+                    }
+                }
+
+                if (this.Count > 0 && selectedRemoved == true)
+                {
+                    this.SelectedItem = this.First();
+                }
+                else
+                {
+                    this.SelectedItem = default(T);
                 }
             }
         }
