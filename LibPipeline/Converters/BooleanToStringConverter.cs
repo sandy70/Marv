51be74baf1e5d002737094123b2d28980ec1c3f2
Vinod Khare <vinod.khare@gmail.com>
2013-08-15 10:09:25 -0400

    Project files.

--- LibPipeline/Converters/BooleanToStringConverter.cs
+++ LibPipeline/Converters/BooleanToStringConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace LibPipeline
+namespace LibPipeline 
 {
     [ValueConversion(typeof(bool), typeof(string))]
     public class BooleanToStringConverter : DependencyObject, IValueConverter
