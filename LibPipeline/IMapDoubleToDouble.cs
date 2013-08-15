e75b4258b670b1e60ef0b189005446d36f0ddc89
Vinod Khare <vinod.khare@gmail.com>
2013-08-15 10:08:23 -0400

    IMapDoubleToDouble: defines an interface that contains methods to map a double value to another double value.

---
+++ LibPipeline/IMapDoubleToDouble.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibPipeline
+{
+    interface IMapDoubleToDouble
+    {
+        double Map(double a);
+        double MapBack(double b);
+    }
+}
