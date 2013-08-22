1440d26e2f2302c315dfc1b761c655c2af44212a
Vinod Khare <vinod.khare@gmail.com>
2013-08-22 16:01:53 -0400

    AdcoInput: functions to read ADCO input file.

---
+++ Marv/AdcoInput.cs
@@ -0,0 +1,96 @@
+﻿using LibPipeline;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Marv
+{
+    public static class AdcoInput
+    {
+        public static int GetColumnIndex(this ExcelWorksheet sheet, string columnName)
+        {
+            // Column indices are 1 based.
+            var columnIndex = 1;
+            var value = sheet.GetValue(1, columnIndex);
+
+            while (value != null)
+            {
+                if (value.Equals(columnName))
+                {
+                    return columnIndex;
+                }
+
+                value = sheet.GetValue(1, ++columnIndex);
+            }
+
+            return -1;
+        }
+
+        public static object GetValue(this ExcelWorksheet sheet, int rowIndex, string columnName)
+        {
+            return sheet.GetValue(rowIndex, sheet.GetColumnIndex(columnName));
+        }
+
+        public static SelectableCollection<MultiLocation> Read()
+        {
+            using (var package = new ExcelPackage(new FileInfo(@"D:\Data\ADCO02\ADCO 5.xlsx")))
+            {
+                var multiLocations = new SelectableCollection<MultiLocation>();
+                var nHeaderRows = 3;
+                var pipelineStartRowIndices = new List<int>();
+                var rowIndex = nHeaderRows + 1;
+                var sheet = package.Workbook.Worksheets.Single(worksheet => worksheet.Name == "data");
+
+                var nameColumnIndex = sheet.GetColumnIndex("name");
+                var name = sheet.GetValue(rowIndex, nameColumnIndex);
+
+                while (name != null)
+                {
+                    if (rowIndex == nHeaderRows + 1 || name != sheet.GetValue(rowIndex - 1, nameColumnIndex))
+                    {
+                        pipelineStartRowIndices.Add(rowIndex);
+                    }
+
+                    name = sheet.GetValue(++rowIndex, nameColumnIndex);
+                }
+
+                pipelineStartRowIndices.Add(rowIndex);
+
+                var pipelineIndex = 1;
+
+                while (pipelineIndex < pipelineStartRowIndices.Count)
+                {
+                    var pipelineStartRowIndex = pipelineStartRowIndices[pipelineIndex - 1];
+                    var pipelineEndRowIndex = pipelineStartRowIndices[pipelineIndex];
+
+                    var multiLocation = new MultiLocation();
+                    multiLocation.Name = sheet.GetValue(pipelineStartRowIndex, "name") as string;
+                    multiLocation["StartYear"] = sheet.GetValue(pipelineStartRowIndex, "START");
+
+                    for (rowIndex = pipelineStartRowIndex; rowIndex < pipelineEndRowIndex; rowIndex++)
+                    {
+                        multiLocation.Add(new Location
+                        {
+                            Latitude = (double)sheet.GetValue(rowIndex, "inlet_latitude"),
+                            Longitude = (double)sheet.GetValue(rowIndex, "inlet_longitude")
+                        });
+                    }
+
+                    multiLocation.Add(new Location
+                    {
+                        Latitude = (double)sheet.GetValue(pipelineEndRowIndex - 1, "outlet_latitude"),
+                        Longitude = (double)sheet.GetValue(pipelineEndRowIndex - 1, "outlet_longitude")
+                    });
+
+                    multiLocations.Add(multiLocation);
+
+                    pipelineIndex++;
+                }
+
+                return multiLocations;
+            }
+        }
+    }
+}
