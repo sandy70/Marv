14ff897b893ec9e7cf4862e6805f3d52de68d89d
Vinod Khare <vinod.khare@gmail.com>
2013-08-22 16:01:15 -0400

    MultiLocation: properties can now be added to multiLocation.

--- LibPipeline/Geometry/MultiLocation.cs
+++ LibPipeline/Geometry/MultiLocation.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace LibPipeline
 {
     public class MultiLocation : SelectableCollection<Location>
     {
+        private Dictionary<string, object> dictionary = new Dictionary<string, object>();
         private string name;
 
         public string Name
@@ -19,6 +22,40 @@ namespace LibPipeline
                 {
                     this.name = value;
                     this.OnPropertyChanged(new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+
+        public object this[string name]
+        {
+            get
+            {
+                if (this.GetType().GetProperties().Where(info => info.Name.Equals(name)).Count() == 0)
+                {
+                    if (dictionary.ContainsKey(name))
+                    {
+                        return dictionary[name];
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    return this.GetType().GetProperty(name).GetValue(this);
+                }
+            }
+
+            set
+            {
+                if (this.GetType().GetProperties().Where(info => info.Name.Equals(name)).Count() == 0)
+                {
+                    dictionary[name] = value;
+                }
+                else
+                {
+                    this.GetType().GetProperty(name).SetValue(this, value);
                 }
             }
         }
