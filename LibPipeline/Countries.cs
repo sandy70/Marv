858b23608f94963f73bd0b72a6bad4aab59379f6
Vinod Khare <vinod.khare@gmail.com>
2013-09-06 13:55:32 -0400

    Cleanup.

--- LibPipeline/Countries.cs
+++ LibPipeline/Countries.cs
@@ -9,5 +9,6 @@ namespace LibPipeline
     public class Countries
     {
         public LocationRect USA = new LocationRect { South = 25, West = -124, North = 50, East = -66 };
+        public LocationRect UAE = new LocationRect { North = 30, East = 60, West = 50, South = 20 };
     }
 }
