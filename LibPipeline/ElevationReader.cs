3d374d00c1a0054b4f0aafedf1c36105a99d0b10
Vinod Khare <vinod.khare@gmail.com>
2013-09-06 13:47:58 -0400

    Cleanup.

--- LibPipeline/ElevationReader.cs
+++ LibPipeline/ElevationReader.cs
@@ -5,20 +5,19 @@ namespace LibPipeline
 {
     public class ElevationReader
     {
-        public double Read(double lat, double lon)
-        {
-            string griddir = (string)ConfigManager.Read("demdb.griddir");
-            string filename = String.Format("n{0:D2}w{1:D3}.hdr", (int)Math.Ceiling(lat), -(int)Math.Floor(lon));
-            string filepath = Path.Combine(griddir, filename);
+        //public double Read(double lat, double lon)
+        //{
+        //    string filename = String.Format("n{0:D2}w{1:D3}.hdr", (int)Math.Ceiling(lat), -(int)Math.Floor(lon));
+        //    string filepath = Path.Combine(griddir, filename);
 
-            GridFloatReader gridFloatReader = new GridFloatReader(filepath);
-            GridFloatHeader gridFloatHeader = gridFloatReader.ReadHeader();
+        //    GridFloatReader gridFloatReader = new GridFloatReader(filepath);
+        //    GridFloatHeader gridFloatHeader = gridFloatReader.ReadHeader();
 
-            int row = (int)Math.Round((lat - gridFloatHeader.YLL) / gridFloatHeader.CellSize);
-            int col = (int)Math.Round((lon - gridFloatHeader.XLL) / gridFloatHeader.CellSize);
-            row = gridFloatHeader.nRows - row;
+        //    int row = (int)Math.Round((lat - gridFloatHeader.YLL) / gridFloatHeader.CellSize);
+        //    int col = (int)Math.Round((lon - gridFloatHeader.XLL) / gridFloatHeader.CellSize);
+        //    row = gridFloatHeader.nRows - row;
 
-            return gridFloatReader.Read(row, col, gridFloatHeader.nCols);
-        }
+        //    return gridFloatReader.Read(row, col, gridFloatHeader.nCols);
+        //}
     }
 }
