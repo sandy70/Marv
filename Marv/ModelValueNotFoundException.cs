924742c9130d96cf3963b89a7db087327a669e88
Vinod Khare <vinod.khare@gmail.com>
2013-09-17 11:09:57 -0400

    ModelValueNotFoundException: should be thrown when ModelValue is not found on disk.

---
+++ Marv/ModelValueNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Marv
+{
+    [Serializable]
+    public class ModelValueNotFoundException : Exception
+    {
+        public ModelValueNotFoundException()
+        {
+        }
+
+        public ModelValueNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public ModelValueNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected ModelValueNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
