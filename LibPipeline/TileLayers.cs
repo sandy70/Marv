af85704067d0ef417cef174dbaf0d15b9d21130c
Vinod Khare <vinod.khare@gmail.com>
2013-09-19 13:56:42 -0400

    TileLayers: adding some new tile layers.

--- LibPipeline/TileLayers.cs
+++ LibPipeline/TileLayers.cs
@@ -4,6 +4,13 @@ namespace LibPipeline
 {
     public static class TileLayers
     {
+        public static TileLayer MapBoxSat = new TileLayer
+        {
+            SourceName = "MapBoxSat",
+            Description = "MapBox Satellite",
+            TileSource = new TileSource("http://a.tiles.mapbox.com/v3/vinodkhare.map-q9bgxflq/{z}/{x}/{y}.png")
+        };
+
         public static TileLayer MapBoxTerrain = new TileLayer
         {
             SourceName = "MapBoxTerrain",
@@ -11,18 +18,18 @@ namespace LibPipeline
             TileSource = new TileSource("http://a.tiles.mapbox.com/v3/vinodkhare.map-vu2qowlx/{z}/{x}/{y}.png")
         };
 
-        public static TileLayer MapQuestSat = new TileLayer
-        {
-            SourceName = "MapQuestSat",
-            Description = "MapQuest Satellite",
-            TileSource = new TileSource("http://otile{n}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png")
-        };
-
         public static TileLayer MapQuestMap = new TileLayer
         {
             SourceName = "MapQuestMap",
             Description = "MapQuest Map",
             TileSource = new TileSource("http://otile{n}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png")
+        };
+
+        public static TileLayer MapQuestSat = new TileLayer
+        {
+            SourceName = "MapQuestSat",
+            Description = "MapQuest Satellite",
+            TileSource = new TileSource("http://otile{n}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png")
         };
 
         public static TileLayer OsmMap = new TileLayer
