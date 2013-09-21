63985a731ff8d536ab23244126df5ad130f2df9d
Vinod Khare <vinod.khare@gmail.com>
2013-09-21 15:03:14 -0400

    Cleanup.

--- LibNetwork/Enums.cs
+++ LibNetwork/Enums.cs
@@ -1,34 +1,9 @@
 ﻿namespace LibNetwork
 {
-    public enum BnInputType
-    {
-        Default,
-        User
-    }
-
-    public enum EvidenceType
-    {
-        SoftEvidence,
-        StateSelected
-    }
-
-    public enum VertexInputType
-    {
-        SoftEvidence,
-        PastYear,
-        StateSelected
-    }
-
     public enum VertexType
     {
         Interval,
         None,
         Number
-    }
-
-    public static class Groups
-    {
-        public static readonly string All = "all";
-        public static readonly string Default = "0";
     }
 }
