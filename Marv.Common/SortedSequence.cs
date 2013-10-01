b46c9f8fe1cb7aed3641520c2eeb29a3efc3d69d
Vinod Khare <vinod.khare@gmail.com>
2013-10-01 16:43:04 -0400

    Telerik added to archive.

---
+++ Marv.Common/SortedSequence.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Marv.Common
+{
+    internal class SortedSequence<T> : ObservableCollection<T> where T : IComparable<T>
+    {
+
+    }
+}
