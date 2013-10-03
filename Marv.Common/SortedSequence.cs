0a70db6c37d17a602039a3ba1f30d0df515f115c
Vinod Khare <vinod.khare@gmail.com>
2013-10-03 15:47:09 -0400

    SortedSequence: models a sorted sequence of comparable quantities.

--- Marv.Common/SortedSequence.cs
+++ Marv.Common/SortedSequence.cs
@@ -1,11 +1,27 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Marv.Common
 {
-    internal class SortedSequence<T> : ObservableCollection<T> where T : IComparable<T>
+    internal class SortedSequence<T> : IEnumerable<T> where T : IComparable<T>
     {
+        private List<T> list = new List<T>();
 
+        public void Add(T item)
+        {
+            this.list.Add(item);
+            this.list.Sort();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.list.GetEnumerator();
+        }
     }
 }
