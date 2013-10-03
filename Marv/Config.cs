c05a0bfcf48eb0118b9c5c627aeaf47557f4dae8
Vinod Khare <vinod.khare@gmail.com>
2013-10-03 12:17:30 -0400

    Config: removed.

--- Marv/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Marv
-{
-    public static class Config
-    {
-        public static int EndYear = 2012;
-        public static string KocDir = @"Resources/Inputs";
-        public static int StartYear = 2004;
-    }
-}
