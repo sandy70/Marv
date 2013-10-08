d76a9c8537214f4d5b8aa47bae6944dca5daefa1
Vinod Khare <vinod.khare@gmail.com>
2013-10-08 14:23:27 -0400

    GraphValue: WriteXlsx() writes the graph value to an excel sheet.

--- LibNetwork/GraphValue.cs
+++ LibNetwork/GraphValue.cs
@@ -1,17 +1,16 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+using NLog;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibNetwork
 {
-    public class GraphValue : Dictionary<string, VertexValue> 
+    public class GraphValue : Dictionary<string, VertexValue>
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public void WriteJson(string fileName)
         {
             var serializer = new JsonSerializer();
@@ -25,6 +24,43 @@ namespace LibNetwork
                     serializer.Serialize(jsonTextWriter, this);
                 }
             }
+        }
+
+        public void WriteXlsx(string fileName)
+        {
+            var excelPackage = new ExcelPackage(new FileInfo(fileName));
+            var excelWorkSheetName = "Sheet1";
+
+            try
+            {
+                excelPackage.Workbook.Worksheets.Add(excelWorkSheetName);
+            }
+            catch (InvalidOperationException exp)
+            {
+                logger.Warn("The worksheet {0} already exists.", excelWorkSheetName);
+            }
+
+            var excelWorkSheet = excelPackage.Workbook.Worksheets[excelWorkSheetName];
+
+            var excelCol = 1;
+
+            foreach (var vertexKey in this.Keys)
+            {
+                var excelRow = 1;
+
+                excelWorkSheet.SetValue(excelRow++, excelCol, vertexKey);
+
+                var vertexValue = this[vertexKey];
+
+                foreach (var stateKey in vertexValue.Keys)
+                {
+                    excelWorkSheet.SetValue(excelRow++, excelCol, vertexValue[stateKey]);
+                }
+
+                excelCol++;
+            }
+
+            excelPackage.Save();
         }
     }
 }
