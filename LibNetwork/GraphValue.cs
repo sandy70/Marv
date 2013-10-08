4b78973816b45626d06d7dce2bfb1ad8f4319778
Vinod Khare <vinod.khare@gmail.com>
2013-10-08 13:20:34 -0400

    GraphValue: WriteJson() writes the graph value in json format.

--- LibNetwork/GraphValue.cs
+++ LibNetwork/GraphValue.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LibNetwork
 {
-    public class GraphValue : Dictionary<string, VertexValue> { }
+    public class GraphValue : Dictionary<string, VertexValue> 
+    {
+        public void WriteJson(string fileName)
+        {
+            var serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+            serializer.Formatting = Formatting.Indented;
+
+            using (var streamWriter = new StreamWriter(fileName))
+            {
+                using (var jsonTextWriter = new JsonTextWriter(streamWriter))
+                {
+                    serializer.Serialize(jsonTextWriter, this);
+                }
+            }
+        }
+    }
 }
