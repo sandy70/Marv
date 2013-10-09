004ff10a63becb552ed8e3f11fd1ee203da06c7c
Vinod Khare <vinod.khare@gmail.com>
2013-10-09 10:14:34 -0400

    Introducing the notification system.

---
+++ Marv/NotificationControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Marv
+{
+    public partial class NotificationControl : UserControl
+    {
+        public static readonly DependencyProperty NotificationsProperty =
+        DependencyProperty.Register("Notifications", typeof(ObservableCollection<NotificationTimed>), typeof(NotificationControl), new PropertyMetadata(new ObservableCollection<NotificationTimed>()));
+
+        public NotificationControl()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<NotificationTimed> Notifications
+        {
+            get { return (ObservableCollection<NotificationTimed>)GetValue(NotificationsProperty); }
+            set { SetValue(NotificationsProperty, value); }
+        }
+
+        public void Add(NotificationTimed notification)
+        {
+            this.Notifications.Add(notification);
+            notification.OnAdded();
+        }
+    }
+}
---
+++ Marv/NotificationTimed.cs
@@ -0,0 +1,76 @@
+﻿using Marv.Common;
+
+namespace Marv
+{
+    public class NotificationTimed : ViewModel, INotification
+    {
+        private double _value = 100;
+        private string description = "";
+        private bool isIndeterminate = false;
+        private string name = "";
+
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+
+            set
+            {
+                if (value != this.description)
+                {
+                    this.description = value;
+                    this.RaisePropertyChanged("Description");
+                }
+            }
+        }
+
+        public bool IsIndeterminate
+        {
+            get
+            {
+                return this.isIndeterminate;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value != this.name)
+                {
+                    this.name = value;
+                    this.RaisePropertyChanged("Name");
+                }
+            }
+        }
+
+        public double Value
+        {
+            get
+            {
+                return this._value;
+            }
+
+            private set
+            {
+                if (value != this._value)
+                {
+                    this._value = value;
+                    this.RaisePropertyChanged("Value");
+                }
+            }
+        }
+
+        public void OnAdded()
+        {
+            // do nothing
+        }
+    }
+}
