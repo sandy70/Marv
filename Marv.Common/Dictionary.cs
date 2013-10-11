8c729f44f96a9e622b212c7ef0d6ab3aac2b2d6a
Vinod Khare <vinod.khare@gmail.com>
2013-10-11 11:18:36 -0400

    Dictionary: a two dimentional dictionary.

---
+++ Marv.Common/Dictionary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Marv.Common
+{
+    public class Dictionary<TRowKey, TColKey, TValue>
+    {
+        private Dictionary<TRowKey, Dictionary<TColKey, TValue>> dictionary = new Dictionary<TRowKey, Dictionary<TColKey, TValue>>();
+
+        public TValue this[TRowKey rowKey, TColKey colKey]
+        {
+            get
+            {
+                return this.dictionary[rowKey][colKey];
+            }
+
+            set
+            {
+                if (!this.dictionary.ContainsKey(rowKey))
+                {
+                    this.dictionary[rowKey] = new Dictionary<TColKey, TValue>();
+                }
+
+                this.dictionary[rowKey][colKey] = value;
+            }
+        }
+    }
+}
