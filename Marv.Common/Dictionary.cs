ebe28bc169b602b58d0738d594ff9fa74992e8a0
Vinod Khare <vinod.khare@gmail.com>
2013-10-22 14:12:27 -0400

    Dictionary: 2D and 3D dictionaries.

--- Marv.Common/Dictionary.cs
+++ Marv.Common/Dictionary.cs
@@ -2,26 +2,6 @@
 
 namespace Marv.Common
 {
-    public class Dictionary<TRowKey, TColKey, TValue>
-    {
-        private Dictionary<TRowKey, Dictionary<TColKey, TValue>> dictionary = new Dictionary<TRowKey, Dictionary<TColKey, TValue>>();
-
-        public TValue this[TRowKey rowKey, TColKey colKey]
-        {
-            get
-            {
-                return this.dictionary[rowKey][colKey];
-            }
-
-            set
-            {
-                if (!this.dictionary.ContainsKey(rowKey))
-                {
-                    this.dictionary[rowKey] = new Dictionary<TColKey, TValue>();
-                }
-
-                this.dictionary[rowKey][colKey] = value;
-            }
-        }
-    }
+    public class Dictionary<T1, T2, TValue> : Dictionary<T1, Dictionary<T2, TValue>> { }
+    public class Dictionary<T1, T2, T3, TValue> : Dictionary<T1, Dictionary<T2, T3, TValue>> { }
 }
