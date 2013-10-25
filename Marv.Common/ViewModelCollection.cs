2408c282f7933c77bb2fb00d05c96d05f31d2e15
Vinod Khare <vinod.khare@gmail.com>
2013-10-25 11:19:45 -0400

    ViewModelCollection: this is like SelectableCollection but has more meaningful naming convention.

---
+++ Marv.Common/ViewModelCollection.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Marv.Common
+{
+    public class ViewModelCollection<T> : ObservableCollection<T> where T : INotifyPropertyChanged
+    {
+        private string name;
+
+        private T selectedItem = default(T);
+
+        public event ValueEventHandler<T> SelectionChanged;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value != this.name)
+                {
+                    this.name = value;
+                    this.RaisePropertyChanged("Name");
+                }
+            }
+        }
+
+        public T SelectedItem
+        {
+            get
+            {
+                return this.selectedItem;
+            }
+
+            set
+            {
+                if (this.Contains(value))
+                {
+                    if (!value.Equals(this.selectedItem))
+                    {
+                        this.selectedItem = value;
+                        this.RaisePropertyChanged("SelectedItem");
+                        this.RaiseSelectionChanged();
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("The value provided for SelectedItem does not exist in the collection.");
+                }
+            }
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnCollectionChanged(e);
+
+            if (e.NewItems != null && this.Count == 1)
+            {
+                this.SelectedItem = this.First();
+            }
+
+            else if (e.OldItems != null)
+            {
+                bool selectedRemoved = false;
+
+                foreach (var item in e.OldItems)
+                {
+                    if (item.Equals(this.SelectedItem))
+                    {
+                        selectedRemoved = true;
+                    }
+                }
+
+                if (this.Count > 0 && selectedRemoved == true)
+                {
+                    this.SelectedItem = this.First();
+                }
+                else
+                {
+                    this.SelectedItem = default(T);
+                }
+            }
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            base.OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void RaiseSelectionChanged()
+        {
+            if (this.SelectionChanged != null)
+            {
+                this.SelectionChanged(this, new ValueEventArgs<T> { Value = this.SelectedItem });
+            }
+        }
+    }
+}
