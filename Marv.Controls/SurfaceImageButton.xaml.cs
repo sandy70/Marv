804b03893d1cc30b38ca5256660229f50ea0e74f
Vinod Khare <vinod.khare@gmail.com>
2013-10-25 11:26:45 -0400

    SurfaceImageButton: cleated.

---
+++ Marv.Controls/SurfaceImageButton.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Surface.Presentation.Controls;
+using System.Windows;
+
+namespace Marv.Controls
+{
+    public partial class SurfaceImageButton : SurfaceButton
+    {
+        public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register("Source", typeof(string), typeof(SurfaceImageButton), new PropertyMetadata("/Marv.Common;component/Resources/Icons/Close.png"));
+
+        public SurfaceImageButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Source
+        {
+            get { return (string)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+    }
+}
