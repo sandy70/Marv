8399bc595802ad1de2701c45505d1214e92e5657
Vinod Khare <vinod.khare@gmail.com>
2013-11-20 13:57:56 -0500

    Marv.Temp: created.

---
+++ Marv.Temp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Marv.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marv.Temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFileName = @"D:\Data\ADCO02\ADCO 7.xlsx";
+            var networkFileName = @"D:\Data\ADCO02\ADCO_08.net";
+
+            var pipelineInput = new PipelineInput(inputFileName);
+            var line = pipelineInput.ReadPipelines()["BU-498"];
+            // var graph = Graph.Read(networkFileName);
+
+            foreach(var location in line)
+            {
+                // var graphEvidence = pipelineInput.GetGraphEvidence(graph);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
