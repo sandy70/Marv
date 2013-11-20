62636a92a8df7112630b6f93d0d23bcc684ca540
Vinod Khare <vinod.khare@gmail.com>
2013-11-20 14:52:30 -0500

    Extensions.Linq: MaxIndex() gets the maximum index of an IEnumerable

--- Marv.Common/Extensions/Extensions.Linq.cs
+++ Marv.Common/Extensions/Extensions.Linq.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Marv.Common
 {
@@ -38,6 +39,24 @@ namespace Marv.Common
                         yield return cache.Dequeue();
                 }
             } while (hasRemainingItems);
+        }
+
+        public static int MaxIndex<T>(this IEnumerable<T> sequence) where T : IComparable<T>
+        {
+            int maxIndex = -1;
+            T maxValue = default(T); // Immediately overwritten anyway
+
+            int index = 0;
+            foreach (T value in sequence)
+            {
+                if (value.CompareTo(maxValue) > 0 || maxIndex == -1)
+                {
+                    maxIndex = index;
+                    maxValue = value;
+                }
+                index++;
+            }
+            return maxIndex;
         }
     }
 }
