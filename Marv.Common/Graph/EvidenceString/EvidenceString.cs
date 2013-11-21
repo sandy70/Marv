7bf686e781184e3408d78777565458330ccc2595
Vinod Khare <vinod.khare@gmail.com>
2013-11-21 14:31:20 -0500

    Evidence string parsing refactored into the EvidenceString system.

---
+++ Marv.Common/Graph/EvidenceString/EvidenceString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marv.Common
+{
+    public abstract class EvidenceString
+    {
+        protected string _string = null;
+
+        public EvidenceString(string aString)
+        {
+            this._string = aString;
+        }
+
+        public abstract IEvidence Parse(Vertex vertex);
+    }
+}
