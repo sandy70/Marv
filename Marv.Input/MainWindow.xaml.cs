e06d25190094dc071bb228c598fd3a23e0727d41
Vinod Khare <vinod.khare@gmail.com>
2014-03-21 13:05:36 -0400

    Marv.Input: begin.

---
+++ Marv.Input/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Telerik.Windows.Controls;
+
+namespace Marv.Input
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            StyleManager.ApplicationTheme = new Windows8TouchTheme();
+
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // var data = new Dictionary<
+        }
+    }
+}
