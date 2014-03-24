87de3e26296d6fcb089283f6cd10cc0938abaa69
Vinod Khare <vinod.khare@gmail.com>
2014-03-24 13:40:38 -0400

    ModelBase: implements INotifyPropertyChanged.

---
+++ Marv.Common/ModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Marv.Common
+{
+    public class ModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
