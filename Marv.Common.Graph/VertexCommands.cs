1f4cdd602a8341ca743cb3d723dc2a1738cb9f9b
Vinod Khare <vinod.khare@gmail.com>
2014-03-26 16:16:29 -0400

    VertexCommands: static class containing the instances of Vertex Commands.

---
+++ Marv.Common.Graph/VertexCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marv.Common.Graph
+{
+    public static class VertexCommands
+    {
+        public static VertexExpandCommandNew VertexExpandCommand = new VertexExpandCommandNew
+        {
+            ImageSource = "/Marv.Common;component/Resources/Icons/Expand.png"
+        };
+
+        public static VertexLockCommandNew VertexLockCommand = new VertexLockCommandNew
+        {
+            ImageSource = "/Marv.Common;component/Resources/Icons/Lock.png"
+        };
+    }
+}
