ff297c91890400b3fb65bd7df98f433e9f3d0c1d
Vinod Khare <vinod.khare@gmail.com>
2014-03-26 16:15:52 -0400

    Command: a lightweight command class.

---
+++ Marv.Common.Graph/Command.cs
@@ -0,0 +1,25 @@
+﻿namespace Marv.Common.Graph
+{
+    public abstract class Command<T> : NotifyPropertyChanged
+    {
+        private string imageSource;
+
+        public string ImageSource
+        {
+            get
+            {
+                return this.imageSource;
+            }
+
+            set
+            {
+                if (value == this.imageSource) return;
+
+                this.imageSource = value;
+                this.RaisePropertyChanged("ImageSource");
+            }
+        }
+
+        public abstract void Excecute(T item);
+    }
+}
