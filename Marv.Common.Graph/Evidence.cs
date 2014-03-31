597501645172c6ad45d2d9b36b6b98dc62d3d13b
Vinod Khare <vinod.khare@gmail.com>
2014-03-31 14:08:14 -0400

    Evidence: models vertex evidence.

---
+++ Marv.Common.Graph/Evidence.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Marv.Common.Graph
+{
+    public class Evidence : Dictionary<string, double>
+    {
+        public string String { get; set; }
+    }
+}
