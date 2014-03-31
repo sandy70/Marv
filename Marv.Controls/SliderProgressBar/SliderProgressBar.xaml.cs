796ed37ae3f33e1e600ff7e7bb6d4c4873524efc
Vinod Khare <vinod.khare@gmail.com>
2014-03-31 10:37:19 -0400

    SliderProgressBar: added IsTextVisibleProperty.

--- Marv.Controls/SliderProgressBar/SliderProgressBar.xaml.cs
+++ Marv.Controls/SliderProgressBar/SliderProgressBar.xaml.cs
@@ -1,42 +1,74 @@
-﻿using Marv.Common;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Media;
-using Telerik.Windows.Controls;
+using Marv.Common;
 
 namespace Marv.Controls
 {
-    public partial class SliderProgressBar : RadSlider
+    public partial class SliderProgressBar
     {
         public static readonly DependencyProperty IsEditableProperty =
-        DependencyProperty.Register("IsEditable", typeof(bool), typeof(SliderProgressBar), new PropertyMetadata(false));
+            DependencyProperty.Register("IsEditable", typeof (bool), typeof (SliderProgressBar), new PropertyMetadata(false));
 
         public static readonly DependencyProperty SliderForegroundProperty =
-        DependencyProperty.Register("SliderForeground", typeof(Brush), typeof(SliderProgressBar), new PropertyMetadata(new SolidColorBrush(Colors.LightGray)));
+            DependencyProperty.Register("SliderForeground", typeof (Brush), typeof (SliderProgressBar), new PropertyMetadata(new SolidColorBrush(Colors.LightGray)));
 
         public static readonly RoutedEvent ValueEnteredEvent =
-        EventManager.RegisterRoutedEvent("ValueEntered", RoutingStrategy.Bubble, typeof(RoutedEventHandler<ValueEventArgs<double>>), typeof(SliderProgressBar));
+            EventManager.RegisterRoutedEvent("ValueEntered", RoutingStrategy.Bubble, typeof (RoutedEventHandler<ValueEventArgs<double>>), typeof (SliderProgressBar));
+
+        public static readonly DependencyProperty IsTextVisibleProperty =
+            DependencyProperty.Register("IsTextVisible", typeof (bool), typeof (SliderProgressBar), new PropertyMetadata(true));
 
         public SliderProgressBar()
         {
             InitializeComponent();
         }
 
-        public event RoutedEventHandler<ValueEventArgs<double>> ValueEntered
-        {
-            add { AddHandler(ValueEnteredEvent, value); }
-            remove { RemoveHandler(ValueEnteredEvent, value); }
-        }
-
         public bool IsEditable
         {
-            get { return (bool)GetValue(IsEditableProperty); }
-            set { SetValue(IsEditableProperty, value); }
+            get
+            {
+                return (bool) GetValue(IsEditableProperty);
+            }
+            set
+            {
+                SetValue(IsEditableProperty, value);
+            }
+        }
+
+        public bool IsTextVisible
+        {
+            get
+            {
+                return (bool) GetValue(IsTextVisibleProperty);
+            }
+            set
+            {
+                SetValue(IsTextVisibleProperty, value);
+            }
         }
 
         public Brush SliderForeground
         {
-            get { return (Brush)GetValue(SliderForegroundProperty); }
-            set { SetValue(SliderForegroundProperty, value); }
+            get
+            {
+                return (Brush) GetValue(SliderForegroundProperty);
+            }
+            set
+            {
+                SetValue(SliderForegroundProperty, value);
+            }
+        }
+
+        public event RoutedEventHandler<ValueEventArgs<double>> ValueEntered
+        {
+            add
+            {
+                AddHandler(ValueEnteredEvent, value);
+            }
+            remove
+            {
+                RemoveHandler(ValueEnteredEvent, value);
+            }
         }
     }
 }
