e158645f3138f685f672d677086c8628fcccca03
Vinod Khare <vinod.khare@gmail.com>
2014-07-11 12:35:33 -0400

    Marv.Input: fixed some crashes.

--- Marv.Input/MainWindow.xaml.cs
+++ Marv.Input/MainWindow.xaml.cs
@@ -123,7 +123,9 @@ namespace Marv.Input
             var row = new Dynamic();
             try
             {
-                row["Section ID"] = "Section " + (this.InputRows.Count + 1);
+                var sectionId = "Section " + (this.InputRows.Count + 1);
+                row["Section ID"] = sectionId;
+                this.LineEvidence[sectionId] = new Dictionary<int, string, VertexEvidence>();
             }
             catch (NullReferenceException)
             {
@@ -188,7 +190,10 @@ namespace Marv.Input
         {
             var inputRows = new ObservableCollection<dynamic>();
             var row = new Dynamic();
-            row["Section ID"] = "Section 1";
+
+            var sectionId = "Section 1";
+            row["Section ID"] = sectionId;
+            this.LineEvidence[sectionId] = new Dictionary<int, string, VertexEvidence>();
 
             for (var year = this.StartYear; year <= this.EndYear; year++)
             {
@@ -197,7 +202,7 @@ namespace Marv.Input
 
             inputRows.Add(row);
             this.InputRows = inputRows;
-            this.LineEvidence = new Dictionary<string, int, string, VertexEvidence>();
+            
             this.Graph.Belief = null;
             this.Graph.SetEvidence(null);
             foreach (var column in this.InputGridView.Columns)
