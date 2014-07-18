6c602a90177c3fdcf7d729bd7b77e2eb29382b95
Vinod Khare <vinod.khare@gmail.com>
2014-07-18 18:38:31 -0400

    SliderProgressBar: cleaner implementation.

--- Marv.Controls/SliderProgressBar/ProgressBarBehavior.cs
+++ Marv.Controls/SliderProgressBar/ProgressBarBehavior.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 using System.Windows.Interactivity;
+using System.Windows.Threading;
 using Marv.Common;
 using Telerik.Windows.Controls;
 
@@ -7,28 +9,55 @@ namespace Marv.Controls
 {
     public class ProgressBarBehavior : Behavior<RadProgressBar>
     {
+        private bool isDoubleClicked;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            this.AssociatedObject.MouseDoubleClick += AssociatedObject_MouseDoubleClick;
+            this.AssociatedObject.MouseDown += AssociatedObject_MouseDown;
+            this.AssociatedObject.MouseMove += AssociatedObject_MouseMove;
+            this.AssociatedObject.MouseUp += AssociatedObject_MouseUp;
+        }
+
+        private void AssociatedObject_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.isDoubleClicked = true;
+            this.AssociatedObject.Value = 100;
+            this.RaiseValueEntered();
+        }
+
         private void AssociatedObject_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.SetValue(sender, e);
-            }
+            this.isDoubleClicked = false;
+            this.SetValue(sender, e);
         }
 
         private void AssociatedObject_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (e.LeftButton == MouseButtonState.Pressed && !this.isDoubleClicked)
             {
                 this.SetValue(sender, e);
                 e.Handled = true;
             }
         }
 
-        protected override void OnAttached()
+        private void AssociatedObject_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            base.OnAttached();
-            this.AssociatedObject.MouseDown += this.AssociatedObject_MouseDown;
-            this.AssociatedObject.MouseMove += AssociatedObject_MouseMove;
+            if (!this.isDoubleClicked)
+            {
+                this.RaiseValueEntered();
+            }
+        }
+
+        private void RaiseValueEntered()
+        {
+            var sliderProgressBar = this.AssociatedObject.FindParent<SliderProgressBar>();
+
+            if (sliderProgressBar == null) return;
+
+            sliderProgressBar.RaiseValueEntered();
         }
 
         private void SetValue(object sender, MouseEventArgs e)
@@ -38,7 +67,7 @@ namespace Marv.Controls
             if (!parent.IsEditable) return;
 
             var progressBar = sender as RadProgressBar;
-            this.AssociatedObject.Value = (e.GetPosition(progressBar).X - 1)/(progressBar.ActualWidth - 2)*100;
+            this.AssociatedObject.Value = (e.GetPosition(progressBar).X - 1) / (progressBar.ActualWidth - 2) * 100;
         }
     }
 }
