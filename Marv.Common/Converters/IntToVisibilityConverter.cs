2ca4ae1ce627b6f0244278d8815792ff50ac9895
Vinod Khare <vinod.khare@gmail.com>
2014-07-18 10:49:25 -0400

    IntToVisibilityConverter: 0 -> Collapsed; Visible otherwise.

---
+++ Marv.Common/Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Marv.Common
+{
+    [ValueConversion(typeof (int), typeof (Visibility))]
+    public class IntToVisibilityConverter : DependencyObject, IValueConverter
+    {
+        public static readonly DependencyProperty IsReversedProperty =
+            DependencyProperty.Register("IsReversed", typeof (bool), typeof (IntToVisibilityConverter), new PropertyMetadata(false));
+
+        public bool IsReversed
+        {
+            get
+            {
+                return (bool) this.GetValue(IsReversedProperty);
+            }
+
+            set
+            {
+                this.SetValue(IsReversedProperty, value);
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var original = (int) value;
+
+            if (original != 0 ^ this.IsReversed)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
