8a8725c45a58b1382faceadef98dd4bbbc4f7ed9
Vinod Khare <vinod.khare@gmail.com>
2014-07-28 13:51:29 -0400

    Mav.Input: reworked the LineEvidence storage.

---
+++ Marv.Input/YearEvidence.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Marv.Common;
+using Marv.Common.Graph;
+using Newtonsoft.Json;
+
+namespace Marv.Input
+{
+    public class YearEvidence : IKey<int>
+    {
+        public readonly Dictionary<string, VertexEvidence> GraphEvidence = new Dictionary<string, VertexEvidence>();
+        public int Year { get; set; }
+
+        [JsonIgnore]
+        public int Key
+        {
+            get
+            {
+                return this.Year;
+            }
+
+            set
+            {
+                this.Year = value;
+            }
+        }
+    }
+}
