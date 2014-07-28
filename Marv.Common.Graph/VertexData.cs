99f8bedb96f412c621643466cf18c24745904a64
Vinod Khare <vinod.khare@gmail.com>
2014-07-28 10:21:53 -0400

    VertexData: deleted.

--- Marv.Common.Graph/VertexData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Marv.Common.Graph
-{
-    public class VertexData
-    {
-        public double[] Belief { get; set; }
-        public double[] Evidence { get; set; }
-        public string String { get; set; }
-    }
-}
