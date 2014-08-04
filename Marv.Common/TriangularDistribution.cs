21dbd001def72f572a150c803fc37de040cac816
Vinod Khare <vinod.khare@gmail.com>
2014-08-04 13:00:39 -0400

    TriangularDistribution: class that represents a triangular distribution.

---
+++ Marv.Common/TriangularDistribution.cs
@@ -0,0 +1,29 @@
+﻿namespace Marv.Common
+{
+    public class TriangularDistribution
+    {
+        public double Max { get; set; }
+        public double Min { get; set; }
+        public double Mode { get; set; }
+
+        public double Pdf(double x)
+        {
+            if (x < this.Min)
+            {
+                return 0;
+            }
+
+            if (this.Min <= x && x <= this.Mode)
+            {
+                return 2 * (x - this.Min) / (this.Max - this.Min) / (this.Mode - this.Min);
+            }
+
+            if (this.Mode <= x && x <= this.Max)
+            {
+                return 2 * (this.Max - x) / (this.Max - this.Min) / (this.Max - this.Mode);
+            }
+
+            return 0;
+        }
+    }
+}
