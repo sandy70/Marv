a1e6736e08c01cdf3e357e94d4adca9b8ff801b9
Vinod Khare <vinod.khare@gmail.com>
2014-08-04 13:38:21 -0400

    Marv.Common: distributions moved to their own folder.

---
+++ Marv.Common/Distributions/TriangularDistribution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Marv.Common
+{
+    public class TriangularDistribution
+    {
+        private readonly double max;
+        private readonly double min;
+        private readonly double mode;
+
+        public TriangularDistribution(double theMin, double theMode, double theMax)
+        {
+            this.min = theMin;
+            this.mode = theMode;
+            this.max = theMax;
+        }
+
+        public double Cdf(double x)
+        {
+            if (x < this.min)
+            {
+                return 0;
+            }
+
+            if (this.min <= x && x <= this.mode)
+            {
+                return Math.Pow(x - this.min, 2) / (this.max - this.min) / (this.mode - this.min);
+            }
+
+            if (this.mode <= x && x <= this.max)
+            {
+                return 1 - Math.Pow(this.max - x, 2) / (this.max - this.min) / (this.max - this.mode);
+            }
+
+            return 1;
+        }
+
+        public double Pdf(double x)
+        {
+            if (x < this.min)
+            {
+                return 0;
+            }
+
+            if (this.min <= x && x <= this.mode)
+            {
+                return 2 * (x - this.min) / (this.max - this.min) / (this.mode - this.min);
+            }
+
+            if (this.mode <= x && x <= this.max)
+            {
+                return 2 * (this.max - x) / (this.max - this.min) / (this.max - this.mode);
+            }
+
+            return 0;
+        }
+    }
+}
