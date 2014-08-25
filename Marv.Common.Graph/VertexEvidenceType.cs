ba81b394768f62f649ed78cad71a4537a17ff118
Vinod Khare <vinod.khare@gmail.com>
2014-08-25 15:56:56 -0400

    VertexEvidenceTypes: type of evidences that can be entered in to a Vertex.

---
+++ Marv.Common.Graph/VertexEvidenceType.cs
@@ -0,0 +1,13 @@
+﻿namespace Marv.Common.Graph
+{
+    public enum VertexEvidenceType
+    {
+        Distribution,
+        Invalid,
+        Normal,
+        Number,
+        Range,
+        State,
+        Triangular
+    }
+}
