337d077ee0dd0b870906523c27137ec19cae4786
Vinod Khare <vinod.khare@gmail.com>
2014-08-26 14:31:24 -0400

    KeyedCollection: collection of IKeyed objects.

---
+++ Marv.Common/IKeyed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marv.Common
+{
+    interface IKeyed
+    {
+        string Key { get; set; }
+    }
+}
---
+++ Marv.Common/KeyedCollection.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Marv.Common
+{
+    public class KeyedCollection<T> : ObservableCollection<T>, IKeyed
+    {
+        private readonly Dictionary<string, T> dictionary = new Dictionary<string, T>();
+        private string key;
+
+        public T this[string aKey]
+        {
+            get
+            {
+                return this.dictionary[aKey];
+            }
+        }
+
+        public string Key
+        {
+            get
+            {
+                return this.key;
+            }
+
+            set
+            {
+                this.key = value;
+                this.OnPropertyChanged(new PropertyChangedEventArgs("Key"));
+            }
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnCollectionChanged(e);
+
+            if (e.OldItems != null)
+            {
+                foreach (var keyedItem in e.OldItems.OfType<IKeyed>())
+                {
+                    this.dictionary.Remove(keyedItem.Key);
+                }
+            }
+
+            if (e.NewItems != null)
+            {
+                foreach (var keyedItem in e.NewItems.OfType<IKeyed>())
+                {
+                    this.dictionary[keyedItem.Key] = (T) keyedItem;
+                }
+            }
+        }
+    }
+}
