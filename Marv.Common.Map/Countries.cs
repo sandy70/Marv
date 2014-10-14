985705d2d32f9e82e5185f743ce25043ede8a8fe
Vinod Khare <vinod.khare@gmail.com>
2014-10-14 10:35:46 -0400

    Cleanup.

--- Marv.Common.Map/Countries.cs
+++ Marv.Common.Map/Countries.cs
@@ -4,15 +4,15 @@ namespace Marv.Map
 {
     public static class Countries
     {
-        public static LocationRect China = new LocationRect { North = 54, East = 135, South = 20, West = 73 };
-        public static LocationRect UAE = new LocationRect { North = 30, East = 60, West = 50, South = 20 };
-        public static LocationRect USA = new LocationRect { South = 25, West = -124, North = 50, East = -66 };
-
-        public static Dictionary<string, LocationRect> BoundsForKey = new Dictionary<string, LocationRect>
+        public static readonly Dictionary<string, LocationRect> BoundsForKey = new Dictionary<string, LocationRect>
         {
-            { "China", Countries.China },
-            { "UAE", Countries.UAE },
-            { "USA", Countries.USA }
+            { "China", China },
+            { "UAE", UAE },
+            { "USA", USA }
         };
+
+        public static readonly LocationRect China = new LocationRect { North = 54, East = 135, South = 20, West = 73 };
+        public static readonly LocationRect UAE = new LocationRect { North = 30, East = 60, West = 50, South = 20 };
+        public static readonly LocationRect USA = new LocationRect { South = 25, West = -124, North = 50, East = -66 };
     }
 }
--- Marv.Common.Map/Properties/AssemblyInfo.cs
+++ Marv.Common.Map/Properties/AssemblyInfo.cs
@@ -1,12 +1,9 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Windows.Markup;
-
-// General Information about an assembly is controlled through the following 
+﻿// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-using System.Xml;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 [assembly: AssemblyTitle("Marv.Common.Map")]
 [assembly: AssemblyDescription("")]
@@ -20,9 +17,11 @@ using System.Xml;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("333e802f-5085-4672-9caa-b665ea4a4456")]
 
 // Version information for an assembly consists of the following four values:
@@ -35,7 +34,7 @@ using System.Xml;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: XmlnsDefinition("http://dnvgl.com/marv", "Marv.Map")]
