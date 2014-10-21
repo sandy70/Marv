48d448ef8f2216c970ba4de3ce19143af49509a9
Vinod Khare <vinod.khare@gmail.com>
2014-10-21 15:34:14 -0400

    Extensions: cleanup.

--- Marv.Controls.Map/Extensions.cs
+++ Marv.Controls.Map/Extensions.cs
@@ -9,7 +9,7 @@ namespace Marv.Controls.Map
     {
         public static IEnumerable<Point> ToPoints(this IEnumerable<Location> locations, MapView mapView)
         {
-            return locations.Select(location => mapView.LocationToViewportPoint(Marv.Map.Extensions.ToMapControlLocation(location)));
+            return locations.Select(location => mapView.LocationToViewportPoint(location.ToMapControlLocation()));
         }
 
         public static IEnumerable<Location> ToLocations(this IEnumerable<Point> points, MapView mapView)
