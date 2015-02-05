9a9720b80dfbd3a37156e0b052c5c35493122c9d
Vinod Khare <vinod.khare@gmail.com>
2015-02-05 10:54:32 -0500

    SectionEvidence: Read() reads a SectionEvidence object from a json file.

--- Marv.Common.Graph/SectionEvidence.cs
+++ Marv.Common.Graph/SectionEvidence.cs
@@ -2,8 +2,16 @@
 
 namespace Marv
 {
+    /// <summary>
+    /// This class exists so that we can interface with MATLAB. Do not try to replace with base class.
+    /// </summary>
     public class SectionEvidence : Dict<int, string, VertexEvidence>
     {
+        public static SectionEvidence Read(string filePath)
+        {
+            return Utils.ReadJson<SectionEvidence>(filePath);
+        }
+
         public void Write(string filePath)
         {
             this.WriteJson(filePath);
