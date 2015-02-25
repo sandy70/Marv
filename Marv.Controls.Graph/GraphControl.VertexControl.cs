3ebef2a6442ae046ceeb7068800a8243a9eb75b7
Vinod Khare <vinod.khare@gmail.com>
2015-02-25 14:03:25 -0500

    GraphControl: VertexControl event handles separated.

---
+++ Marv.Controls.Graph/GraphControl.VertexControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Input;
+using Marv.Common;
+using Marv.Controls.Graph;
+
+namespace Marv.Controls
+{
+    public partial class GraphControl
+    {
+        private void VertexControl_CommandExecuted(object sender, Command<VertexControl> command)
+        {
+            this.Graph.SelectedVertex = (sender as VertexControl).Vertex;
+
+            if (command == VertexControlCommands.Expand)
+            {
+                this.UpdateLayout();
+            }
+            else if (command == VertexControlCommands.SubGraph)
+            {
+                this.SelectedGroup = (sender as VertexControl).Vertex.HeaderOfGroup;
+            }
+        }
+
+        private void VertexControl_EvidenceEntered(object sender, VertexEvidence e)
+        {
+            this.RaiseEvidenceEntered(e);
+        }
+
+        private void VertexControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as VertexControl).IsToolbarVisible = true;
+        }
+
+        private void VertexControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as VertexControl).IsToolbarVisible = false;
+        }
+    }
+}
--- Marv.Controls.Graph/GraphControl.xaml.cs
+++ Marv.Controls.Graph/GraphControl.xaml.cs
@@ -572,35 +572,6 @@ namespace Marv.Controls
             this.DisplayVertexKey = vertexKey;
         }
 
-        private void VertexControl_CommandExecuted(object sender, Command<VertexControl> command)
-        {
-            this.Graph.SelectedVertex = (sender as VertexControl).Vertex;
-
-            if (command == VertexControlCommands.Expand)
-            {
-                this.UpdateLayout();
-            }
-            else if (command == VertexControlCommands.SubGraph)
-            {
-                this.SelectedGroup = (sender as VertexControl).Vertex.HeaderOfGroup;
-            }
-        }
-
-        private void VertexControl_EvidenceEntered(object sender, VertexEvidence e)
-        {
-            this.RaiseEvidenceEntered(e);
-        }
-
-        private void VertexControl_MouseEnter(object sender, MouseEventArgs e)
-        {
-            (sender as VertexControl).IsToolbarVisible = true;
-        }
-
-        private void VertexControl_MouseLeave(object sender, MouseEventArgs e)
-        {
-            (sender as VertexControl).IsToolbarVisible = false;
-        }
-
         private void WriteEvidences(string filePath)
         {
             if (Path.GetExtension(filePath) == ".hcs")
