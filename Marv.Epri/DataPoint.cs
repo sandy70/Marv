5b8cf241257ce407a10f220f2353063a0f517983
Vinod Khare <vinod.khare@gmail.com>
2015-04-09 12:25:23 -0400

    Marv.Epri: add classes to model the REST API response.

---
+++ Marv.Epri/DataPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Marv.Epri
+{
+    public class DataPoint
+    {
+        public string id { get; set; }
+        public int quality { get; set; }
+        public DateTime server_timestamp { get; set; }
+        public string stream_id { get; set; }
+        public DateTime timestamp { get; set; }
+        public string value { get; set; }
+    }
+}
---
+++ Marv.Epri/Response.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Marv.Epri
+{
+    public class Response
+    {
+        public int count { get; set; }
+        public string cursor { get; set; }
+        public List<DataPoint> list { get; set; }
+        public string next_uri { get; set; }
+        public int size { get; set; }
+    }
+}
