bd34c2641df421f401a561cae997735b73e5879d
Vinod Khare <vinod.khare@gmail.com>
2015-04-10 12:56:23 -0400

    Basic data download now working.

--- Marv.Epri/DataPoint.cs
+++ Marv.Epri/DataPoint.cs
@@ -1,15 +1,49 @@
 ﻿using System;
+using Marv.Common;
 
 namespace Marv.Epri
 {
     // Do not change the property names here because they must match the JSON response
-    public class DataPoint
+    public class DataPoint : NotifyPropertyChanged
     {
+        private DateTime server_timestamp;
+        private int value;
+
+        public DateTime Server_TimeStamp
+        {
+            get { return this.server_timestamp; }
+
+            set
+            {
+                if (value.Equals(this.server_timestamp))
+                {
+                    return;
+                }
+
+                this.server_timestamp = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public int Value
+        {
+            get { return this.value; }
+
+            set
+            {
+                if (value.Equals(this.value))
+                {
+                    return;
+                }
+
+                this.value = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
         public string id { get; set; }
         public int quality { get; set; }
-        public DateTime server_timestamp { get; set; }
         public string stream_id { get; set; }
         public DateTime timestamp { get; set; }
-        public int value { get; set; }
     }
 }
--- Marv.Epri/MainWindow.xaml.cs
+++ Marv.Epri/MainWindow.xaml.cs
@@ -1,41 +1,176 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Net;
 using System.Net.Http;
-using System.Text;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Marv.Common;
+using Marv.Common.Types;
+using Marv.Epri.Properties;
 using Newtonsoft.Json;
 
 namespace Marv.Epri
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : INotifyPropertyChanged
     {
+        private ObservableCollection<DataPoint> dataPoints = new ObservableCollection<DataPoint>();
+
+        private NotificationCollection notifications = new NotificationCollection();
+
+        private TimeSpan selectedTimeSpan;
+
+        private Dict<string, TimeSpan> timespans = new Dict<string, TimeSpan>
+        {
+            { "1 Hour", TimeSpan.FromHours(1) },
+            { "6 Hours", TimeSpan.FromHours(6) },
+            { "1 Day", TimeSpan.FromDays(1) },
+            { "7 Days", TimeSpan.FromDays(7) }
+        };
+
+        public ObservableCollection<DataPoint> DataPoints
+        {
+            get { return this.dataPoints; }
+
+            set
+            {
+                if (value.Equals(this.dataPoints))
+                {
+                    return;
+                }
+
+                this.dataPoints = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public NotificationCollection Notifications
+        {
+            get { return this.notifications; }
+
+            set
+            {
+                if (value.Equals(this.notifications))
+                {
+                    return;
+                }
+
+                this.notifications = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public TimeSpan SelectedTimeSpan
+        {
+            get { return this.selectedTimeSpan; }
+
+            set
+            {
+                if (value.Equals(this.selectedTimeSpan))
+                {
+                    return;
+                }
+
+                this.selectedTimeSpan = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public Dict<string, TimeSpan> TimeSpans
+        {
+            get { return this.timespans; }
+
+            set
+            {
+                if (value.Equals(this.timespans))
+                {
+                    return;
+                }
+
+                this.timespans = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        private async void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            const string uri = @"http://devicecloud.digi.com/ws/v1/streams/history/00000000-00000000-00409DFF-FF88AC6C/Sensor01.json?timeline=server&order=desc&size=50";
-
-            using (var httpClient = new HttpClient(new HttpClientHandler { Credentials = new NetworkCredential("vkhare13", "!Ncc1764") }))
+            var notification = new Notification
             {
-                var response = await JsonConvert.DeserializeObjectAsync<Response>(await httpClient.GetStringAsync(uri));
-                Console.WriteLine(response);
+                IsIndeterminate = true,
+                Description = "Loading..."
+            };
+
+            this.Notifications.Add(notification);
+
+            this.DataPoints = await this.DownloadDataPointsAsync();
+
+            this.Notifications.Remove(notification);
+        }
+
+        private async Task<ObservableCollection<DataPoint>> DownloadDataPointsAsync()
+        {
+            const string server = @"http://devicecloud.digi.com";
+
+            const string uriEndPoint = server + "/ws/v1/streams/history/00000000-00000000-00409DFF-FF88AC6C/Sensor01.json";
+
+            var uri = uriEndPoint + "?" + Utils.FormRestArgs(new
+            {
+                start_time = (DateTime.UtcNow - this.SelectedTimeSpan).ToString("o"),
+                timeline = "server"
+            });
+
+            using (var httpClient = new HttpClient(new HttpClientHandler { Credentials = new NetworkCredential(Settings.Default.Login, Settings.Default.Password) }))
+            {
+                var downloadedPoints = new ObservableCollection<DataPoint>();
+
+                while (true)
+                {
+                    var response = await Task.Run(async () => JsonConvert.DeserializeObject<Response>(await httpClient.GetStringAsync(uri)));
+
+                    downloadedPoints.Add(response.list);
+
+                    if (response.next_uri == null)
+                    {
+                        break;
+                    }
+
+                    uri = server + response.next_uri;
+                }
+
+                return downloadedPoints;
             }
         }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var notification = new Notification
+            {
+                IsIndeterminate = true,
+                Description = "Loading..."
+            };
+
+            this.Notifications.Add(notification);
+
+            this.DataPoints = await this.DownloadDataPointsAsync();
+
+            this.Notifications.Remove(notification);
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (this.PropertyChanged != null && propertyName.Length > 0)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
