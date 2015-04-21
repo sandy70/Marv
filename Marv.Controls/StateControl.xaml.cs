564ceac7dd530302a891776fe19603d3a6cb9fa5
Vinod Khare <vinod.khare@gmail.com>
2015-04-21 16:59:27 -0400

    StateControl: control to display a state.

---
+++ Marv.Controls/StateControl.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+
+namespace Marv.Controls
+{
+    public partial class StateControl
+    {
+        public static readonly DependencyProperty IsEditableProperty = DependencyProperty.Register("IsEditable", typeof (bool), typeof (StateControl), new PropertyMetadata(false));
+        public static readonly DependencyProperty IsEvidenceVisibleProperty = DependencyProperty.Register("IsEvidenceVisible", typeof (bool), typeof (StateControl), new PropertyMetadata(true));
+        public static readonly DependencyProperty IsValueVisibleProperty = DependencyProperty.Register("IsValueVisible", typeof (bool), typeof (StateControl), new PropertyMetadata(true));
+
+        public bool IsEditable
+        {
+            get { return (bool) GetValue(IsEditableProperty); }
+            set { SetValue(IsEditableProperty, value); }
+        }
+
+        public bool IsEvidenceVisible
+        {
+            get { return (bool) GetValue(IsEvidenceVisibleProperty); }
+            set { SetValue(IsEvidenceVisibleProperty, value); }
+        }
+
+        public bool IsValueVisible
+        {
+            get { return (bool) GetValue(IsValueVisibleProperty); }
+            set { SetValue(IsValueVisibleProperty, value); }
+        }
+
+        public StateControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RaiseValueEntered(double value)
+        {
+            if (this.ValueEntered != null)
+            {
+                this.ValueEntered(this, value);
+            }
+        }
+
+        private void SliderProgressBar_OnValueEntered(object sender, double value)
+        {
+            this.RaiseValueEntered(value);
+        }
+
+        public event EventHandler<double> ValueEntered;
+    }
+}
