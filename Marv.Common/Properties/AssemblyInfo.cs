13175c9a92bcf2917d2d4b6f87190a0e1da467e5
Vinod Khare <vinod.khare@gmail.com>
2015-05-11 11:07:45 -0400

    Fix references.

--- Marv.Common/Properties/AssemblyInfo.cs
+++ Marv.Common/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Windows.Markup;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("52965437-655d-4a71-8c8d-b34057e07db5")]
