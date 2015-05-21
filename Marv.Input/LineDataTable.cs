1b24178687803bfd57fb96127e576a0ff0b6df79
unknown <skomm@CLOD405595.verit.dnv.com>
2015-05-21 11:40:52 -0400

    validation for section,from and to entries

---
+++ Marv.Input/LineDataTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marv.Input
+{
+    public class LineDataTable : DataTable
+    {
+        public LineDataTable(string tableName) : base(tableName)
+        {
+            this.RowChanged +=LineDataTable_RowChanged;
+           
+        }
+
+        private void LineDataTable_RowChanged(object sender, DataRowChangeEventArgs e)
+        {
+            if (e.Action == DataRowAction.Add)
+            {
+                var index = this.Rows.IndexOf(e.Row);
+
+                if (index == 0)
+                {
+                    e.Row["From"] = 0;
+                    e.Row["To"] = 0;
+                }
+                else
+                {
+                    var previousRow = this.Rows[index - 1];
+                    e.Row["From"] = previousRow["To"];
+                    e.Row["To"] = previousRow["To"];
+                }
+            }
+
+        }
+
+    }
+}
