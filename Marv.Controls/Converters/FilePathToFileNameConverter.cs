4555ce053b12a85f0032cc51423eb6650a3095d2
swamy sandeep kommuru <swamy.kommuru@dnvgl.com>
2015-05-28 09:25:47 -0400

    Filepathtofileconverter added

--- Marv.Controls/Converters/FilePathToFileNameConverter.cs
+++ Marv.Controls/Converters/FilePathToFileNameConverter.cs
@@ -16,14 +16,7 @@ namespace Marv.Controls.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-      /*       if (this.Network.FileName != null)
-           {
-               char [] delimiter= {'\\'};
-               string [] folders= this.Network.FileName.Split(delimiter);
-               string filename = folders[folders.Length - 1];
-               this.Network.FileName = filename;
-           }*/
-
+    
             var filename = "";
 
                 if (value.ToString() != "")
