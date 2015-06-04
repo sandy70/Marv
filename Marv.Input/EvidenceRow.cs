dcdae92e918e46cd5087e7a385519dbc254b7ab6
Vinod Khare <vinod.khare@gmail.com>
2015-06-04 16:39:41 -0400

    EvidenceRow: models a single row of evidence in the evidence grid view.

---
+++ Marv.Input/EvidenceRow.cs
@@ -0,0 +1,42 @@
+﻿using Marv.Common.Types;
+
+namespace Marv.Input
+{
+    public class EvidenceRow : Dynamic
+    {
+        private double? from;
+        private double? to;
+
+        public double? From
+        {
+            get { return this.from; }
+
+            set
+            {
+                if (value.Equals(this.from))
+                {
+                    return;
+                }
+
+                this.from = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public double? To
+        {
+            get { return this.to; }
+
+            set
+            {
+                if (value.Equals(this.to))
+                {
+                    return;
+                }
+
+                this.to = value;
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+}
