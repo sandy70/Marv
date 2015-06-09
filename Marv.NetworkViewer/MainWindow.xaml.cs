cda10a3cfc3e33b91692838683adbfca618e5c15
Vinod Khare <vinod.khare@gmail.com>
2015-06-09 13:21:32 -0400

    Marv.NetworkViewer: add Notifications on network open.

--- Marv.NetworkViewer/MainWindow.xaml.cs
+++ Marv.NetworkViewer/MainWindow.xaml.cs
@@ -1,21 +1,24 @@
-﻿using System.IO;
-using System.Windows;
-using Marv.NetworkViewer.Properties;
+﻿using System.Windows;
+using Marv.Common;
 using Telerik.Windows.Controls;
 
 namespace Marv.NetworkViewer
 {
     public partial class MainWindow
     {
+        public static readonly DependencyProperty NotificationsProperty =
+            DependencyProperty.Register("Notifications", typeof (NotificationCollection), typeof (MainWindow), new PropertyMetadata(new NotificationCollection()));
+
+        public NotificationCollection Notifications
+        {
+            get { return (NotificationCollection) GetValue(NotificationsProperty); }
+            set { SetValue(NotificationsProperty, value); }
+        }
+
         public MainWindow()
         {
             StyleManager.ApplicationTheme = new Windows8Theme();
             InitializeComponent();
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }
