dcb7fb0e68d3f886e301fdbfa5f14abac833bbaf
Vinod Khare <vinod.khare@gmail.com>
2015-06-09 11:10:33 -0400

    Marv.Network: app to load and run a single network.

---
+++ Marv.NetworkViewer/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Telerik.Windows.Controls;
+
+namespace Marv.NetworkViewer
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            StyleManager.ApplicationTheme = new Windows8Theme();
+            InitializeComponent();
+        }
+    }
+}
