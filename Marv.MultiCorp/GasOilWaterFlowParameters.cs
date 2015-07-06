a35fb3dfe2054f7029205ead5be38e37600523c8
Vinod Khare <vinod.khare@gmail.com>
2015-07-06 14:22:46 -0400

    Marv.MultiCorp: write interface functions for MATLAB.

---
+++ Marv.MultiCorp/GasOilWaterFlowParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace Marv.MultiCorp
+{
+    public class GasOilWaterFlowParameters
+    {
+        public double GasDensity = 7.54; // kg / m^3
+        public double Inclination = 0; // deg
+        public double InternalDiameter = 0.1; // m
+        public double MixtureVelocity = 1; // m/s
+        public double OilDensity = 825; // kg/m^3
+        public double OilViscosity = 0.002; // Pa.s
+        public double SuperficialGasVelocity = 2; // m/s
+        public double WaterCut = 0.05; // %
+    }
+}
