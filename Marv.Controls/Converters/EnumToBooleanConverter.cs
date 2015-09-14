b54115c8995aabb2185eac99c7695dfb46ff96d8
swamy sandeep kommuru <swamy.kommuru@dnvgl.com>
2015-09-14 11:39:55 -0400

    converter added

---
+++ Marv.Controls/Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Marv.Controls.Converters
+{
+    public class EnumToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
